604f56f46c077eca0643f0821351e2092c6760b0
KaliuzhnyiArtem <kaliuzhnyi.artem@gmail.com>
2022-10-06 19:29:10 +0300

    make migrations

--- shortLinks/urlshorded/models.py
+++ shortLinks/urlshorded/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    username = models.TextField()
+    password = models.TextField()
+
+
+class Guests(models.Model):
+    ip = models.TextField()
+
+
+class Links(models.Model):
+    main_links = models.TextField(blank=False)
+    short_links = models.TextField(blank=False)
+    deleted = models.BooleanField()
+    owner_id = models.ForeignKey('UserInfo', on_delete=models.PROTECT, null=True)
+    owner_ip = models.ForeignKey('Guests', on_delete=models.PROTECT, null=True)
+
+
+class ClickToLinks(models.Model):
+    id_links = models.ForeignKey('Links', on_delete=models.PROTECT)
+    ip_visitor = models.TextField(blank=False)
+    id_country = models.ForeignKey('Country', on_delete=models.PROTECT)
+    date = models.DateField()
+
+
+class Country(models.Model):
+    name = models.TextField(blank=False)
